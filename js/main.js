5ccb1170050e804dfdd5d8b7ade5903fc1548799
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2017-02-04 03:14:23 +1100

    Big change to design

--- js/main.js
+++ js/main.js
@@ -1,192 +1,172 @@
-( function() {
-  var displayAboutMe = function() {
-    $( "#aboutme" ).on( "click", function() {
-      $( "#contacts" ).removeClass( "nav-selected" );
-      $( "#projects" ).removeClass( "nav-selected" );
-      $( "#aboutme" ).addClass( "nav-selected" );
+console.log( "Hey! You've found me in the console." );
+console.log( "There are a few projects that I made throughout my Web Development Immersive course, plus more which are not listed." );
+console.log( "If you're interested in getting in touch with me:" );
+console.log( "Email: emmanuel.fallancy@gmail.com" );
+console.log( "Twitter: @fancyfallancy" );
+console.log( "LinkedIn: linkedin.com/in/emmanuelfallancy" );
 
-      var content = $( ".content" );
-      content.html( "" ); // Clear the content before appending info to it
+$( document ).ready( function() {
 
-      var context = $( "<div></div>" ).addClass( "context" ).html( "<p>Hi there and hello!</p><p>Just recently, I finished my Web Development (Immersive) Course at General Assembly in Sydney. I am on the hunt for an adventure in my career as a Web Developer.</p><p>I have a background in software testing and used to be a consultant that heavily focus on programming. Weird ey? I joined Quintiq as a Test Engineer and later transition into an Application Consultant (the one that I mentioned before), due to my joyful excitement in programming.</p><p>Nonetheless, I have passion in Web Development and therefore decided to go on an adventure to pursue on a role as Web Developer.</p><p>I have a deep interest in big data and machine learning. I love to tinker with technology in my spare time and outside of coding, I enjoy strong coffees with toasted banana bread!</p>");
+  // Contact me section
+  $( ".span-contacts" ).hover( function() {
 
-      $( ".content" ).append( context )
+    $( "body" ).css( {
+      "background-image": "url( 'assets/images/contacts_1.gif' )",
+      "background-repeat": "no-repeat",
+      "background-size": "100% 100%"
     } );
-  };
 
-  var displayProjects = function() {
-    $( "#projects" ).on( "click", function() {
-      $( "#contacts" ).removeClass( "nav-selected" );
-      $( "#projects" ).addClass( "nav-selected" );
-      $( "#aboutme" ).removeClass( "nav-selected" );
-
-      var content = $( ".content" );
-      content.html( "" ); // Clear the content before appending info to it
-
-      var context = $( "<div></div>" ).addClass( "context" );
-      var projectdetails = $( "<div></div>" ).addClass( "project-details" );
-
-      var message = $( "<p></p>" ).html( "Oops! Still under maintenance and will be ready soon!" );
-
-      var notes = $( "<p></p>" ).html( "For projects related, you can go check out my github in my github profile by navigating to contacts section and click on Github." );
-
-      projectdetails.append( message );
-      projectdetails.append( notes );
-      context.append( projectdetails );
-      content.append( context );
+    $( ".span-contacts" ).css( "opacity", "1" );
+    $( ".span-contacts a" ).css( {
+      "font-size": "42px",
+      "color": "#dcdcdc",
+      "text-shadow": "2px 2px #000000",
+      "font-style": "italic",
+      "text-decoration": "none",
+      "font-weight": "900"
     } );
-  };
 
-  var displayContacts = function() {
-    $( "#contacts" ).on( "click", function() {
-      $( "#contacts" ).addClass( "nav-selected" );
-      $( "#projects" ).removeClass( "nav-selected" );
-      $( "#aboutme" ).removeClass( "nav-selected" );
-
-      var content = $( ".content" );
-      content.html( "" ); // Clear the content before appending info to it
-
-      var contactdetails = $( "<div></div>" ).addClass( "contact-details" );
-
-      var message = $( "<p></p>" ).html( "Let's get in touch and have a chat." );
-
-      var githublink = '<a href="https://github.com/emmafallancy" target="_blank">Github</a>';
-      var github = $( "<button></button>" ).html( githublink );
-
-      var linkedinlink = '<a href="https://linkedin.com/in/emmanuelfallancy" target="_blank">LinkedIn</a>'
-      var linkedin = $( "<button></button>" ).html( linkedinlink );
-
-      contactdetails.append( message );
-      contactdetails.append( github );
-      contactdetails.append( $( "<br>" ) );
-      contactdetails.append( linkedin );
-
-      var context = $( "<div></div>" ).addClass( "context" );
-
-      context.append( contactdetails );
-      content.append( context );
-
+    $( ".span-contacts a" ).html( "\"Coffee? Contact me.\"" );
+    $( ".span-about" ).css( "opacity", "0" );
+    $( ".span-projects" ).css( "opacity", "0" );
+    $( ".span-linkedin" ).css( "opacity", "0" );
+    $( ".span-opac" ).css( "opacity", "0" );
+    $( ".nav a" ).css( {
+      "color": "#ffffff",
+      "font-weight": "900",
+      "background-color": "rgba( 0, 0, 0, 0.45)"
     } );
-  };
 
-  var displayGreeting = function() {
-    var content = $( ".content" );
-    content.html( "" ); // Clear the content before appending info to it
+  }, function() {
+    $( "body" ).css( {
+      "background-image": "",
+      "background-repeat": "",
+      "background-size": ""
+    } );
 
-    var date = new Date();
-    currentDate = date.getDate();
-    currentHours = date.getHours();
+    $( ".span-contacts a" ).html( "coffee and banana bread" );
+    $( ".span-contacts a" ).css( {
+      "font-size": "",
+      "color": "",
+      "text-shadow": "",
+      "font-style": "",
+      "text-decoration": "",
+      "font-weight": ""
+    } );
+    $( ".span-about" ).css( "opacity", "1" );
+    $( ".span-projects" ).css( "opacity", "1" );
+    $( ".span-opac" ).css( "opacity", "1" );
+    $( ".nav a" ).css( {
+      "color": "",
+      "font-weight": "",
+      "text-shadow": "",
+      "background-color": ""
+    } );
 
-    var month = date.getMonth();
-    var monthStr = "";
-
-    var year = date.getUTCFullYear();
-
-    switch( month ) {
-      case 0:
-        monthStr = "Jan";
-        break;
-      case 1:
-        monthStr = "Feb";
-        break;
-      case 2:
-        monthStr = "Mar";
-        break;
-      case 3:
-        monthStr = "Apr";
-        break;
-      case 4:
-        monthStr = "May";
-        break;
-      case 5:
-        monthStr = "Jun";
-        break;
-      case 6:
-        monthStr = "Jul";
-        break;
-      case 7:
-        monthStr = "Aug";
-        break;
-      case 8:
-        monthStr = "Sep";
-        break;
-      case 9:
-        monthStr = "Oct";
-        break;
-      case 10:
-        monthStr = "Nov";
-        break;
-      case 11:
-        monthStr = "Dec";
-        break;
-      default:
-        monthStr = "Something is wrong";
-        break;
-    }
-
-    var greetings = $( "<div></div>" ).addClass( "greetings" );
-
-    if( currentHours >= 0 && currentHours < 6 ) {
-
-      var firstMessage = $( "<p></p>" ).html( "Oh, hello there! You're awake." );
-
-      var secondMessage = $( "<p></p>" ).html( "It's " + currentDate + " " + monthStr + " " + year );
-
-      var thirdMessage = $( "<p></p>" ).html( "Good (early) morning and have a joyful day ahead!" );
-
-      greetings.append( firstMessage );
-      greetings.append( secondMessage );
-      greetings.append( thirdMessage );
-
-    } else if( currentHours >= 6 && currentHours < 12 ) {
-
-      var firstMessage = $( "<p></p>" ).html( "In case if you are still having hangover, today is Monday and the date?" );
-
-      var secondMessage = $( "<p></p>" ).html( "It's " + currentDate + " " + monthStr + " " + year );
-
-      var thirdMessage = $( "<p></p>" ).html( "Good morning and have an awesome day ahead!" );
-
-      greetings.append( firstMessage );
-      greetings.append( secondMessage );
-      greetings.append( thirdMessage );
-
-    } else if( currentHours >= 12 && currentHours < 18 ) {
-
-      var firstMessage = $( "<p></p>" ).html( "It's afternoon and you're almost there!" );
-
-      var secondMessage = $( "<p></p>" ).html( "It's " + currentDate + " " + monthStr + " " + year );
-
-      var thirdMessage = $( "<p></p>" ).html( "Good afternoon and don't stop being awesome!" );
-
-      greetings.append( firstMessage );
-      greetings.append( secondMessage );
-      greetings.append( thirdMessage );
-
-    } else if( currentHours >= 18 && currentHours < 24 ) {
-
-      var firstMessage = $( "<p></p>" ).html( "You've made it through the day!" );
-
-      var secondMessage = $( "<p></p>" ).html( "It's " + currentDate + " " + monthStr + " " + year );
-
-      var thirdMessage = $( "<p></p>" ).html( "Good evening and have a great rest!" );
-
-      greetings.append( firstMessage );
-      greetings.append( secondMessage );
-      greetings.append( thirdMessage );
-
-    }
-
-    var context = $( "<div></div>" ).addClass( "context" ).html( "" );
-    context.append( greetings );
-    $( ".content" ).append( context )
-  }
-
-  $( document ).ready( function() {
-
-    displayGreeting();
-    displayAboutMe();
-    displayProjects();
-    displayContacts();
   } );
 
-} )();
+  // About section
+  $( ".span-about" ).hover( function() {
+    $( "body" ).css( {
+      "background-image": "url( 'assets/images/fancy.jpg' )",
+      "background-repeat": "no-repeat",
+      "background-size": "cover"
+    } );
+
+    $( ".span-about" ).css( "opacity", "1" );
+    $( ".span-about a" ).css( {
+      "font-size": "42px",
+      "color": "#dcdcdc",
+      "text-shadow": "2px 2px #000000",
+      "font-style": "italic",
+      "text-decoration": "none",
+      "font-weight": "900"
+    } );
+
+    $( ".span-about a" ).html( "Get to know me." );
+    $( ".span-contacts" ).css( "opacity", "0" );
+    $( ".span-projects" ).css( "opacity", "0" );
+    $( ".span-linkedin" ).css( "opacity", "0" );
+    $( ".span-opac" ).css( "opacity", "0" );
+
+  }, function() {
+    $( "body" ).css( {
+      "background-image": "",
+      "background-repeat": "",
+      "background-size": ""
+    } );
+
+    $( ".span-about a" ).html( "Emmanuel" );
+    $( ".span-about a" ).css( {
+      "font-size": "",
+      "color": "",
+      "text-shadow": "",
+      "font-style": "",
+      "text-decoration": "",
+      "font-weight": ""
+    } );
+    $( ".span-contacts" ).css( "opacity", "1" );
+    $( ".span-projects" ).css( "opacity", "1" );
+    $( ".span-opac" ).css( "opacity", "1" );
+
+  });
+
+  // Projects section
+  $( ".span-projects" ).hover( function() {
+    $( "body" ).css( {
+      "background-image": "url( 'assets/images/projects_1.gif' )",
+      "background-repeat": "no-repeat",
+      "background-size": "100% 100%"
+    } );
+
+    $( ".span-projects" ).css( "opacity", "1" );
+    $( ".span-projects a" ).css( {
+      "font-size": "42px",
+      "color": "#dcdcdc",
+      "text-shadow": "2px 2px #000000",
+      "font-style": "italic",
+      "text-decoration": "none",
+      "font-weight": "900"
+    } );
+
+    $( ".span-projects a" ).html( "See my work." );
+    $( ".span-contacts" ).css( "opacity", "0" );
+    $( ".span-about" ).css( "opacity", "0" );
+    $( ".span-linkedin" ).css( "opacity", "0" );
+    $( ".span-opac" ).css( "opacity", "0" );
+
+    $( ".nav a" ).css( {
+      "color": "#ffffff",
+      "font-style": "italic",
+      "font-weight": "900",
+      "text-shadow": "2px 2px #000000"
+    } );
+
+  }, function() {
+    $( "body" ).css( {
+      "background-image": "",
+      "background-repeat": "",
+      "background-size": ""
+    } );
+
+    $( ".span-projects a" ).html( "Web Developer" );
+    $( ".span-projects a" ).css( {
+      "font-size": "",
+      "color": "",
+      "text-shadow": "",
+      "font-style": "",
+      "text-decoration": "",
+      "font-weight": ""
+    } );
+    $( ".span-contacts" ).css( "opacity", "1" );
+    $( ".span-about" ).css( "opacity", "1" );
+    $( ".span-opac" ).css( "opacity", "1" );
+
+    $( ".nav a" ).css( {
+      "color": "",
+      "font-style": "",
+      "font-weight": "",
+      "text-shadow": ""
+    } );
+  })
+});
